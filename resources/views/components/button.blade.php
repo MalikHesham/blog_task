264504475527a835eb90b55d64ef5be61eb1fcb6
Malik Hesham <malikhesham@outlook.com>
2021-03-20 17:27:55 +0200

    fixing comment issues

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,3 @@
 <span>
-    //for making it dynamically changing we added {$slot}
-    <a href="{{ $href }}" class="btn btn-{{ $type }}">{{$slot}}</a>
+        <a href="{{ $href }}" class="btn btn-{{ $type }}">{{$slot}}</a>
 </span>
