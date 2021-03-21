1f7512c9c181ca49ad1eb940eb9031f33725675d
Malik Hesham <malikhesham@outlook.com>
2021-03-21 21:38:20 +0200

    solving delete button bug

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -33,12 +33,11 @@
                 <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal{{$post->id}}">
                 Delete
                 </button>
-
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            </td>
+                <div class="modal fade" id="exampleModal{{$post->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModal{{$post->id}}">confirm delete</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
@@ -57,7 +56,6 @@
                         </div>
                     </div>
                 </div>
-            </td>
         </tr>
     @endforeach
     </tbody>
